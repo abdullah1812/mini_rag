6ce574cd9ab9efd333eb6a3d186f91771400a200
abdullah18h <dzer9181@gmail.com>
2025-12-02 17:43:13 +0200

    move application to src

--- src/routes/base.py
+++ src/routes/base.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, APIRouter
 
 
 base_route = APIRouter(
-    prefix="/base/v1", # prefix خاص بالراوت دا 
+    # prefix="/base/v1", # prefix خاص بالراوت دا 
     tags=['base_v1'] # اسم خاص بالراوت دا بس
 )
 
