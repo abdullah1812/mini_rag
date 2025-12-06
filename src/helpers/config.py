b4130849cbf36c950dce99ed32bfdbe2c33a246c
abdullah18h <dzer9181@gmail.com>
2025-12-07 00:44:27 +0200

    build helpers files

---
+++ src/helpers/config.py
@@ -0,0 +1,21 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+
+    APP_NAME:str
+    APP_VERSION:str
+
+    FILE_AVAILABLE_TYPES:list
+    FILE_MAX_SIZE:int
+    FILE_DEFAULT_CHUNK_SIZE:int
+
+
+    model_config = SettingsConfigDict(
+        env_file="./.env",
+        env_file_encoding="utf-8",
+        extra="ignore"
+    )
+
+def get_settings():
+    return Settings()
